0bd569a9c41c1c41b59c37fb96fa49e0437e6410
Sheikh Sayed <sksayed@live.com>
2019-04-29 07:13:47 +0600

    sql file is added

--- estore.sql
+++ estore.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2018 at 09:00 PM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
+-- Generation Time: Apr 29, 2019 at 03:12 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -49,7 +49,8 @@ CREATE TABLE `admins` (
 INSERT INTO `admins` (`id`, `name`, `username`, `email`, `phone`, `password`, `gender`, `dob`, `nid`, `presentaddress`, `parmanentaddress`) VALUES
 (1, 'Noyon', 'shanto', 'shanto@gmail.com', '', '1234567890', 'male', '1996-05-24', '19979972342363842', 'H-23,R-4,Nikunja-2,Khilkhet,Dhaka', 'Dhaka'),
 (2, 'arnob', 'arnob', 'ar@gmIL.COM', '', '1234567890', 'male', '9843-12-12', '3245465756543', 'wgbnb,hstr', 'geawaiuobvgair,gaer'),
-(3, 'noyon', 'noyon', 'noyon892@gmail.com', '', '1234567890', 'Male', '2018-03-18', '19979972342363842', 'Mahajon Bari,Affratpara,Chatmohar,Pabna', 'Dhaka');
+(3, 'noyon', 'noyon', 'noyon892@gmail.com', '', '1234567890', 'Male', '2018-03-18', '19979972342363842', 'Mahajon Bari,Affratpara,Chatmohar,Pabna', 'Dhaka'),
+(4, 'sayed rahman', 'sayed', 'sayedrahman015@gmail.com', '01670420393', 'sayed1122', 'male', '2019-04-02', '1325467498', 'bashundhara', 'bashundhara');
 
 -- --------------------------------------------------------
 
@@ -67,10 +68,11 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `categoryname`) VALUES
-(1, 'Men'),
+(1, 'Mens'),
 (2, 'Women'),
 (3, 'Food'),
-(4, 'ELectronics');
+(4, 'ELectronics'),
+(7, 'Medicines');
 
 -- --------------------------------------------------------
 
@@ -95,7 +97,9 @@ INSERT INTO `orders` (`orderId`, `customerId`) VALUES
 (123474, 36),
 (123475, 36),
 (123476, 36),
-(123477, 36);
+(123477, 36),
+(123478, 39),
+(123479, 39);
 
 -- --------------------------------------------------------
 
@@ -114,7 +118,7 @@ CREATE TABLE `products` (
   `image3` varchar(256) DEFAULT NULL,
   `details` varchar(8192) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `rank` int(11) NOT NULL
+  `rank` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -122,12 +126,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `productname`, `price`, `quantity`, `categoryId`, `image1`, `image2`, `image3`, `details`, `date`, `rank`) VALUES
-(10, 'Men\'s Printed Panjabi', 1995, 1800, 1, NULL, NULL, NULL, 'New COllection', '0000-00-00 00:00:00', 2),
-(11, 'Women Pant', 3000, 1305, 2, NULL, NULL, NULL, 'New Collection', '2018-03-19 06:16:11', 3),
-(12, 'iPhone', 100000, 893, 4, NULL, NULL, NULL, 'New Collection', '2018-03-19 06:17:03', 1),
-(13, 'Biskut', 20, 2395, 3, NULL, NULL, NULL, 'Nice Biskut', '2018-03-19 06:17:49', 1),
-(14, 'Women Shirt', 2500, 1905, 2, NULL, NULL, NULL, 'Nice\r\n', '2018-03-19 06:18:29', 7),
-(15, 'Mobile', 2000, 4, 1, NULL, NULL, NULL, 'asdfghjk', '2018-04-23 05:54:40', 5);
+(10, 'Men\'s Printed Panjabi', 1259, 1845, 1, NULL, NULL, NULL, 'New COllection', '0000-00-00 00:00:00', 5),
+(11, 'Women Pant', 3000, 1304, 2, NULL, NULL, NULL, 'New Collection', '2018-03-19 06:16:11', 8),
+(12, 'iPhone', 100000, 893, 4, NULL, NULL, NULL, 'New Collection', '2018-03-19 06:17:03', 2),
+(13, 'Biskut', 20, 2394, 3, NULL, NULL, NULL, 'Nice Biskut', '2018-03-19 06:17:49', 5),
+(14, 'Women Shirt', 2500, 1904, 2, NULL, NULL, NULL, 'Nice\r\n', '2018-03-19 06:18:29', 17),
+(15, 'Mobile', 2000, 4, 1, NULL, NULL, NULL, 'asdfghjk', '2018-04-23 05:54:40', 8),
+(19, 'wallet', 96000, 500, 1, NULL, NULL, NULL, 'ok', '2019-04-28 22:13:20', 0);
 
 -- --------------------------------------------------------
 
@@ -156,16 +161,19 @@ CREATE TABLE `soldproduct` (
 --
 
 INSERT INTO `soldproduct` (`id`, `productid`, `productname`, `categoryId`, `orderId`, `username`, `quantity`, `price`, `phonenumber`, `address`, `zipcode`, `delivery`, `Orderdate`) VALUES
-(123456, 10, 'Men\'s Printed Panjabi', 1, 123466, 'noyon', 1, 1995, '01733844422', 'Dhaka', '1229', 'no', '2018-04-23 15:56:55'),
+(123456, 10, 'Men\'s Printed Panjabi', 1, 123466, 'noyon', 1, 1995, '01733844422', 'Dhaka', '1229', 'yes', '2018-04-23 15:56:55'),
 (123457, 14, 'Women Shirt', 2, 123466, 'noyon', 1, 2500, '01733844422', 'Dhaka', '1229', 'no', '2018-04-23 15:56:55'),
 (123458, 10, 'Men\'s Printed Panjabi', 1, 123467, 'noyon', 1, 1995, '01733844422', 'Dhaka', '1229', 'no', '2018-04-23 15:57:27'),
 (123459, 14, 'Women Shirt', 2, 123467, 'noyon', 1, 2500, '01733844422', 'Dhaka', '1229', 'no', '2018-04-23 15:57:27'),
 (123460, 12, 'iPhone', 4, 123468, 'noyon', 1, 100000, '01733844422', 'Dhaka', '1229', 'no', '2018-04-23 16:02:16'),
 (123465, 10, 'Men\'s Printed Panjabi', 1, 123473, 'noyon', 1, 1995, '01733844422', 'Dhaka', '1229', 'no', '2018-04-25 21:04:02'),
-(123466, 15, 'Mobile', 1, 123473, 'noyon', 1, 2000, '01733844422', 'Dhaka', '1229', 'no', '2018-04-25 21:04:03'),
+(123466, 15, 'Mobile', 1, 123473, 'noyon', 1, 2000, '01733844422', 'Dhaka', '1229', 'yes', '2018-04-25 21:04:03'),
 (123467, 11, 'Women Pant', 2, 123475, 'noyon', 95, 285000, '01733844422', 'Dhaka', '1229', 'no', '2018-04-25 21:31:49'),
 (123468, 10, 'Men\'s Printed Panjabi', 1, 123476, 'noyon', 70, 139650, '01733844422', 'Dhaka', '1229', 'no', '2018-04-25 21:33:18'),
-(123469, 15, 'Mobile', 1, 123477, 'noyon', 1, 2000, '01733844422', 'Dhaka', '1229', 'no', '2018-04-25 22:14:39');
+(123469, 15, 'Mobile', 1, 123477, 'noyon', 1, 2000, '01733844422', 'Dhaka', '1229', 'yes', '2018-04-25 22:14:39'),
+(123470, 14, 'Women Shirt', 2, 123478, 'mustakim', 1, 2500, '015577889963', 'House:xxx,Road:123 ,Dhaka,Bangladesh', '1229', 'no', '2019-04-28 23:23:38'),
+(123471, 11, 'Women Pant', 2, 123479, 'mustakim', 1, 3000, '015577889963', 'House:xxx,Road:123 ,Dhaka,Bangladesh', '1229', 'no', '2019-04-28 23:42:59'),
+(123472, 13, 'Biskut', 3, 123479, 'mustakim', 1, 20, '015577889963', 'House:xxx,Road:123 ,Dhaka,Bangladesh', '1229', 'no', '2019-04-28 23:42:59');
 
 -- --------------------------------------------------------
 
@@ -190,7 +198,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `phone`, `address`) VALUES
 (36, 'noyon', 'noyon', 'noyon892@gmail.com', '1234567890', '01733844422', 'Dhaka'),
 (37, 'noyon', 'noyon892', 'noyon892@gmail.cm', '1234567890', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
-(38, 'Md. Hasan Uzzaman', 'n', 'noyon892@gmail.c', '1234567890', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna');
+(39, 'mustakim', 'mustakim', 'mustakim@gmail.com', '12345', '015577889963', 'House:xxx,Road:123 ctg,Bangladesh');
 
 --
 -- Indexes for dumped tables
@@ -244,32 +252,38 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123478;
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123480;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
 --
 -- AUTO_INCREMENT for table `soldproduct`
 --
 ALTER TABLE `soldproduct`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123470;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123473;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
